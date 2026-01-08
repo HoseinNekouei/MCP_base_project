e5c0986cdadb087635c515bc3a9f7e17a4b9086e
Hossein Nekouei <nekouei.hosein@gmail.com>
2026-01-08 10:41:13 +0330

    implemented prompt func

--- server.py
+++ server.py
@@ -10,7 +10,19 @@ def get_weather(location: str)-> str:
     """ Get currect weather for spesfic location"""
     return f"weather in {location}: Sunny, 72 F"
 
-
-def get_recource(location :str)-> str:
+# Resource Implementation
+@mcp.resource("weather://{location}")
+def get_recource(location: str)-> str:
     """Provide weather data as a resource"""
     return f"Weather data for {location}: Sunny, 72 F"
+
+@mcp.prompt()
+def weather_report(location: str) -> str:
+    """ Create a weather report prompt"""
+    return f"""You are a weather reporter. Weather report for {location}"""
+
+
+# Run the server
+if __name__== "main":
+    mcp.run()
+
